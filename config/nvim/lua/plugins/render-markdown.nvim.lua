8652103a2bf795dfb64728b49c6529ffc2671819
Erfan <erfan@pop-os.localdomain>
2025-04-05 22:19:21 +0330

    add: markdown plugin

---
+++ config/nvim/lua/plugins/render-markdown.nvim.lua
@@ -0,0 +1,14 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    after = { 'nvim-treesitter' },
+    requires = { 'echasnovski/mini.nvim', opt = true }, -- if you use the mini.nvim suite
+    -- requires = { 'echasnovski/mini.icons', opt = true }, -- if you use standalone mini plugins
+    -- requires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
+    config = function()
+        require('render-markdown').setup({
+            completions = { blink = { enabled = true } },
+        })
+
+        vim.keymap.set("n", "<leader>m", ":RenderMarkdown toggle<CR>")
+    end,
+}
