162e34d3ac572eb5be228420030de530e558a561
Erfan Safari <erfansafari111@gmail.com>
2026-05-31 09:33:18 +0330

    add: nixos unstable and upgrade

--- configuration.sample.nix
+++ configuration.sample.nix
@@ -4,6 +4,9 @@
 
 { config, pkgs, ... }:
 
+let
+  unstable = import <nixpkgs-unstable> {};
+in
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -204,6 +207,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
+  system.stateVersion = "26.05"; # Did you read the comment?
 
 }
